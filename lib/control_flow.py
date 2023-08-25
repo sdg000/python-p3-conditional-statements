e8324b8d5bec698ff5f12890b107b930edd7cf55
Seth David Gyimah <esec26@gmail.com>
2023-08-25 13:08:00 +0000

    solved all but stupid 1

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -1,17 +1,58 @@
 #!/usr/bin/env python3
 
+
 def admin_login(username, password):
-    # your code here
-    pass
+    # USING TERNARY TO DETERMINE IF username and password match VALUES
+    results = (
+        "Access granted"
+        if username.lower() == "admin" and password == "12345"
+        else "Access denied"
+    )
+    return results
+
 
 def hows_the_weather(temperature):
-    # your code here
-    pass
+    if int(temperature) < 40:
+        return "It's brisk out there!"
+
+    # elif int(65 <= temperature > 40):
+    elif int(temperature > 40) and int(temperature <= 65):
+        return "It's a little chilly out there!"
+
+    elif int(temperature > 85):
+        return "It's too dang hot out there!"
+
+    else:
+        return "It's perfect out there!"
+
 
 def fizzbuzz(num):
-    # your code here
-    pass
+    if int(num % 3 == 0) and int(num % 5 == 0):
+        return "FizzBuzz"
+
+    elif int(num % 5) == 0:
+        return "Buzz"
+
+    elif int(num % 3) == 0:
+        return "Fizz"
+
+    else:
+        return num
+
 
 def calculator(operation, num1, num2):
-    # your code here
-    pass
+    if operation == "/":
+        return num1 / num2
+
+    elif operation == "+":
+        return num1 + num2
+
+    elif operation == "*":
+        return num1 * num2
+
+    elif operation == "-":
+        return num1 - num2
+
+    else:
+        print("Invalid operation!\n")
+        return None
